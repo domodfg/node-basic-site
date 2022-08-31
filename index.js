0c5ae1d518629c2387d89600874fba41f50dafc3
domodfg <domodfg@yahoo.com.hk>
2022-08-31 18:51:44 +0800

    Add 404 page redirect

--- index.js
+++ index.js
@@ -2,6 +2,14 @@ const http = require("http");
 const url = require("url");
 const fs = require("fs");
 
+let page404;
+fs.readFile("./404.html", (err, data) => {
+  if (err) {
+    console.log("error");
+  }
+  page404 = data;
+});
+
 http
   .createServer(function (req, res) {
     const q = url.parse(req.url, true);
@@ -10,7 +18,8 @@ http
     fs.readFile(filename, function (err, data) {
       if (err) {
         res.writeHead(404, { "Content-Type": "text/html" });
-        return res.end("404 Not Found");
+        res.write(page404);
+        return res.end();
       }
       res.writeHead(200, { "Content-Type": "text/html" });
       res.write(data);
